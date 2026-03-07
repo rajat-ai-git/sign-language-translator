cd5c887554c17cbf3624ec3c27bbbf24a98a8885
rajatbhiogade <rajatbhiogade@email.com>
2026-03-07 12:57:11 +0530

    Day 11: Real-time prediction working, full pipeline complete

--- predict.py
+++ predict.py
@@ -1,25 +1,30 @@
 import cv2
-import csv
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import csv
+import pickle
+
+with open('model.pkl', 'rb')as f:
+    model = pickle.load(f)
+
+
 img = cv2.VideoCapture(0)
+
 base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
 detector = vision.HandLandmarker.create_from_options(options)
-label = 'No'
+
 while True:
     isTrue, frame = img.read()
-    
 
     if not isTrue:
         break
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
     results = detector.detect(mp_image)
-    key = cv2.waitKey(20) & 0xFF
     if results.hand_landmarks:
-        row=[]
+        row = []
 
         for hand in results.hand_landmarks:
             for landmark in hand:
@@ -31,20 +36,13 @@ while True:
                 row.append(landmark.z)
                 cv2.circle(frame, (x, y), 5, (255, 255, 0), -1)
                
+        prediction = model.predict([row])
+        cv2.putText(frame, str(prediction[0]), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
+    cv2.imshow('video', frame)
 
-
-        if key == ord('s'):
-            with open('data.csv', mode = "a", newline="") as f:
-                writer = csv.writer(f)
-                writer.writerow([label]+row)
-                print("Saved: ", label)
-    
-    cv2.imshow('video', frame)     
-    if key ==ord("q"):
-            break
-
-
-
-
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
 img.release()
 cv2.destroyAllWindows()
+
